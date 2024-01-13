442406a964e8b97b7a492d583c3c30b26af2c38f
Christian Programmer i e "Dreamer <117502862+175M3H3RE@users.noreply.github.com>
2024-01-13 16:19:18 +0530

    Update del_temp.bat

--- del_temp.bat
+++ del_temp.bat
@@ -2,24 +2,22 @@
  set exdirec092893934=
  set file_found09812132=1
  set hidden_29831212982=false
- set str=%1
- if exist %str% (pushd %str% 2>NUL)
- if exist %str% (if "%errorlevel%"=="0" (echo This is a directory.&goto :hehe))
- for /f "tokens=1,2 delims= " %%i in ('dir %str% 2^>NUL') do set exdirec092893934=%%i %%j
+ set str_jkdjahiells=%1
+ if exist %str_jkdjahiells% (pushd %str_jkdjahiells% 2>NUL)
+ if exist %str_jkdjahiells% (if "%errorlevel%"=="0" (echo This is a directory.&goto :heheKLdKlask))
+ for /f "tokens=1,2 delims= " %%i in ('dir %str_jkdjahiells% 2^>NUL') do set exdirec092893934=%%i %%j
  if "%exdirec092893934%"=="0 Dir(s)" (set file_found09812132=0)
- if %file_found09812132%==0 echo File Found!&goto :next
- if "%file_found09812132%" NEQ "0" for /f "tokens=1,2 delims= " %%i in ('dir /ah %str% 2^>NUL') do set exdirec092893934=%%i %%j
- if NOT EXIST %str% echo. echo. >NUL& echo. ------^>%str%^<-------&echo. Please check&goto :eof
+ if %file_found09812132%==0 echo File Found!&goto :next094821933
+ if "%file_found09812132%" NEQ "0" for /f "tokens=1,2 delims= " %%i in ('dir /ah %str_jkdjahiells% 2^>NUL') do set exdirec092893934=%%i %%j
+ if NOT EXIST %str_jkdjahiells% echo. echo. >NUL& echo. ------^>%str_jkdjahiells%^<-------&echo. Please check&goto :eof
  if "%exdirec092893934%"=="0 Dir(s)" (set file_found09812132=0)
  if %file_found09812132%==0 (echo File is Hidden,&goto :eof) else (goto :eof)
- :next
- if exist %str% for /f "delims=" %%i in ('dir /s /b /a-d %str% 2^>NUL') do call :delete "%%i"
- goto :hehe
+ :next094821933
+ if exist %str_jkdjahiells% for /f "delims=" %%i in ('dir /s /b /a-d %str_jkdjahiells% 2^>NUL') do call :delete "%%i"
+ goto :heheKLdKlask
  :delete
  del /p %1
  if not exist %1 echo.File Deleted.
  Exit /B
- :hidden
- Echo. Yaan File is hidden.......please solve it
- :hehe
+ :heheKLdKlask
  popd
