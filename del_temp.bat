d9501fcdbf5ea8bc6852d879df17ba4f9150a056
Christian Programmer i e "Dreamer <117502862+175M3H3RE@users.noreply.github.com>
2024-01-13 16:16:00 +0530

    Update del_temp.bat

--- del_temp.bat
+++ del_temp.bat
@@ -1,17 +1,17 @@
  @echo off
- set exdirec=
- set file_found=1
+ set exdirec092893934=
+ set file_found0981213209812132=1
  set hidden_29831212982=false
  set str=%1
  if exist %str% (pushd %str% 2>NUL)
  if exist %str% (if "%errorlevel%"=="0" (echo This is a directory.&goto :hehe))
- for /f "tokens=1,2 delims= " %%i in ('dir %str% 2^>NUL') do set exdirec=%%i %%j
- if "%exdirec%"=="0 Dir(s)" (set file_found=0)
- if %file_found%==0 echo File Found!&goto :next
- if "%file_found%" NEQ "0" for /f "tokens=1,2 delims= " %%i in ('dir /ah %str% 2^>NUL') do set exdirec=%%i %%j
+ for /f "tokens=1,2 delims= " %%i in ('dir %str% 2^>NUL') do set exdirec092893934=%%i %%j
+ if "%exdirec092893934%"=="0 Dir(s)" (set file_found09812132=0)
+ if %file_found09812132%==0 echo File Found!&goto :next
+ if "%file_found09812132%" NEQ "0" for /f "tokens=1,2 delims= " %%i in ('dir /ah %str% 2^>NUL') do set exdirec092893934=%%i %%j
  if NOT EXIST %str% echo. echo. >NUL& echo. ------^>%str%^<-------&echo. Please check&goto :eof
- if "%exdirec%"=="0 Dir(s)" (set file_found=0)
- if %file_found%==0 (echo File is Hidden,&goto :eof) else (goto :eof)
+ if "%exdirec092893934%"=="0 Dir(s)" (set file_found09812132=0)
+ if %file_found09812132%==0 (echo File is Hidden,&goto :eof) else (goto :eof)
  :next
  if exist %str% for /f "delims=" %%i in ('dir /s /b /a-d %str% 2^>NUL') do call :delete "%%i"
  goto :hehe
