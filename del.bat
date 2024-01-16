6109e6db8f641e6884aead474c9e0937b4670ff0
Christian Programmer "Dreamer <117502862+175M3H3RE@users.noreply.github.com>
2024-01-16 18:32:32 +0530

    Add files via upload

---
+++ del.bat
@@ -0,0 +1,46 @@
+REM %1 hahahahaha
+@echo off
+title REM
+
+echo.
+
+echo.--File Deletion is Disabled-- Press [Z]
+
+echo.
+
+echo.
+echo. 
+choice /n /c YNZ /d y /t 1 >NUL
+if %errorlevel%==3 goto :del_temp
+goto :eof
+:del_temp
+REM echo.Running del_temp
+ @echo off
+ set exdirec=
+ set file_found=1
+ set str=%1
+
+ if exist %str% (pushd %str% 2>NUL)
+ set /a err_level=%errorlevel%
+ popd
+ if exist %str% if "%err_level%"=="0" (echo This is a directory.&goto :hehe)
+ for /f "tokens=1,2 delims= " %%i in ('dir %str% 2^>NUL') do set exdirec=%%i %%j
+ if "%exdirec%"=="0 Dir(s)" (set file_found=0)
+ if %file_found%==0 echo File Found!&goto :next
+ if "%file_found%" NEQ "0" for /f "tokens=1,2 delims= " %%i in ('dir /ah %str% 2^>NUL') do set exdirec=%%i %%j
+ if NOT EXIST %str% echo. echo. >NUL& echo. ------^>%str%^<-------&echo. Please check&goto :eof
+ if "%exdirec%"=="0 Dir(s)" (set file_found=0)
+ if %file_found%==0 (echo File is Hidden,&goto :eof) else (goto :eof)
+ :next
+ REM if exist %str% for /f "delims=" %%i in ('dir /s /b /a-d %str% 2^>NUL') do echo Running delete "%%i"&set filetodelete="%%i"
+ if exist %str% goto :delete
+ goto :hehe
+ :delete
+ if %1=="%userprofile%\Desktop\del.bat" echo.This File can not be deleted.&goto :eof
+ if %1=="%userprofile%\Desktop\del_temp.bat" echo.This File can not be deleted.&goto :eof
+ del /p %1
+ echo %errorlevel%
+ if not exist %1 (if %errorlevel%==0 (echo.File Deleted.) else (echo.Error finding file,)) else (echo.Cancelled.&echo.--^>Checking ..&if exist %1 echo.Found! ----^>%1 )
+ Exit /B
+ :hehe
+ popd
