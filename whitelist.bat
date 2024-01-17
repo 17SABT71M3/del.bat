b8daba32ad03ba99ef18350cebead2bc510c35f5
Christian Programmer "Dr. Dream$ <117502862+175M3H3RE@users.noreply.github.com>
2024-01-17 13:49:16 +0530

    Create whitelist.bat

---
+++ whitelist.bat
@@ -0,0 +1,15 @@
+@echo off
+set /a count=-1
+set /a counter=0
+for /f "delims=" %%i in ('dir /b /s /a-d %1 ') do set /a counter+=1
+if %counter%==0 echo.Unable to Index File & goto :eof
+set /a index=counter-1
+:loop
+set /a count+=1
+if %count%==0 for /f "delims=" %%i in ('dir /b /s /a-d %1 2^>NUL') do set file_name="%%i"
+if %count% GTR 0 for /f "skip=%count% delims=" %%i in ('dir /b /s /a-d %1 2^>NUL') do set file_name="%%i"&goto :next
+:next
+echo add file to whitelist ? %file_name% 
+choice /c yn
+if %errorlevel%==1 type "%userprofile%\desktop\whitelist.txt" | find /i %file_name% >NUL&& echo. >NUL || echo %file_name% >> "%userprofile%\desktop\whitelist.txt"
+if %count% LSS %index% goto loop
