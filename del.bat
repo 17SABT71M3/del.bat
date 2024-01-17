4b952d7727a00055c9d90d5fbf02d9f561ff5852
Christian Programmer "Dr. Dream$ <117502862+175M3H3RE@users.noreply.github.com>
2024-01-17 23:14:22 +0530

    Add files via upload

--- del.bat
+++ del.bat
@@ -1,11 +1,15 @@
 @echo off
+set /a whitelist_Exists=0
+set /a blacklist_Exists=0
 set str=%1
 set arg=%2
 if defined arg if "%arg%"=="?" set file_return="%~fp1"
 if defined arg if "%arg%"=="?" echo %file_return%&goto :eof
 for /f "delims=" %%i in ('"%~fp0" %str% ?') DO set file_name=%%i
 set /a blacklist=0
-if defined str if exist "%userprofile%\desktop\blacklist.txt" set /a blacklist=1&type "%userprofile%\desktop\blacklist.txt" | findstr /n /c:%file_name% &&(echo.MATCHES BLACKLIST&goto :del_temp)
+if exist "%userprofile%\desktop\blacklist.txt" set /a blacklist_exists=1
+if exist "%userprofile%\desktop\blacklist.txt" for /f "delims=" %%i in ('dir "%userprofile%\desktop\blacklist.txt" /ah') do set /a  blacklist_exists=1
+if defined str if %blacklist_exists%==1 set /a blacklist=1&type "%userprofile%\desktop\blacklist.txt" | findstr /n /c:%file_name% &&(echo.MATCHES BLACKLIST&goto :del_temp)
 REM echo."%~fp0" %1
 
 
@@ -43,7 +47,9 @@ REM echo.Running del_temp
  if exist %str% goto :delete
  goto :hehe
  :delete
- if exist "%userprofile%\desktop\whitelist.txt"  type "%userprofile%\desktop\whitelist.txt" | find /i %file_name% >NUL&&(echo.This File can not be deleted.&goto :eof)
+ if not exist "%userprofile%\desktop\whitelist.txt"  for /f "delims=" %%i in ('dir "%userprofile%\desktop\whitelist.txt" /ah') do set /a whitelist_Exists=1
+ if exist "%userprofile%\desktop\whitelist.txt" set /a whitelist_Exists=1
+ if %whiteList_Exists%==1 type "%userprofile%\desktop\whitelist.txt" | find /i %file_name% >NUL&&(echo.This File can not be deleted.&goto :eof)
  if %1=="%userprofile%\Desktop\del.bat" echo.This File can not be deleted.&goto :eof
  if %1=="%userprofile%\Desktop\del_temp.bat" echo.This File can not be deleted.&goto :eof
  :deletealready
